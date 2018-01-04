be5ffb781d0c610d6b3fd9574287a59457263c52
fan <fan@seactrl.com>
2018-01-04 10:58:10 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace getTxtLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files;
+            string line1;
+            string folderPath;
+            if (args.Length>0)
+            {
+                folderPath = args[0];
+            }
+            else
+            {
+                folderPath = @"G:\project\ENC文件加硬件锁\projects\武汉理工大学";
+            }
+            List<string> licenses = new List<string>();
+            files =Directory.GetFiles(folderPath, "*.txt", SearchOption.AllDirectories);
+            foreach (string item in files)
+            {
+                line1 = File.ReadAllLines(item)[0];
+                licenses.Add(line1);
+                Console.WriteLine(line1);
+                File.WriteAllLines(Directory.GetCurrentDirectory() + @"\L1.txt", licenses);
+
+            }
+            Console.WriteLine(folderPath);
+            Console.WriteLine("Press any key to continue...");
+            //Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.ReadKey(true);
+            
+        }
+    }
+}
